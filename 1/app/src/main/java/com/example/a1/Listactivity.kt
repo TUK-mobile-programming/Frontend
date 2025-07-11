86bc60fd1d1c59417d49946a3ff05e42610cb6f4
softronaldo <leejahee220@gmail.com>
2025-07-11 12:09:11 +0900

    초안2

--- 1/app/src/main/java/com/example/a1/Listactivity.kt
+++ 1/app/src/main/java/com/example/a1/Listactivity.kt
@@ -3,6 +3,7 @@ package com.example.a1
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -10,5 +11,11 @@ class Listactivity : AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_listactivity)
+
+            val backButton: AppCompatImageView = findViewById(R.id.listed_back2)
+
+            backButton.setOnClickListener {
+                finish()
+            }
         }
     }
